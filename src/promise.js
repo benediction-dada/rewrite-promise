6ab3024d586d22e44ef97bfb1fb169d01228fe71
yuda <3529878904@qq.com>
2022-11-13 16:12:24 +0800

    Promise then 函数

--- src/promise.js
+++ src/promise.js
@@ -2,10 +2,12 @@ const PENDING = 'pending'
 const FULLFILLED = 'fullfilled'
 const REJECTED = 'rejected'
 
-
 class MyPromise {
     PromiseState = PENDING
     PromiseResult = undefined
+
+    fullfulledCallbacks = []
+    rejectedCallbacks = []
 
     constructor(executor) {
         try {
@@ -15,14 +17,60 @@ class MyPromise {
         }
     }
     resolve = (val) => {
-        if(this.PromiseState !== PENDING) return
+        if (this.PromiseState !== PENDING) return
         this.PromiseState = FULLFILLED
         this.PromiseResult = val
-        return this
+        while (this.fullfulledCallbacks.length) {
+            this.fullfulledCallbacks.shift()()
+        }
     }
     reject = (reason) => {
-        if(this.PromiseState !== PENDING) return 
+        if (this.PromiseState !== PENDING) return
         this.PromiseState = REJECTED
         this.PromiseResult = reason
+        while (this.rejectedCallbacks.length) {
+            this.rejectedCallbacks.shift()()
+        }
+    }
+    then(onFulfilled, onRejected) {
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+
+        const promiseThen = new MyPromise((resolve, reject) => {
+            const callbackPromiseHandler = callback => {
+                queueMicrotask(() => {
+                    try {
+                        const temp = callback(this.PromiseResult)
+                        if (temp === promiseThen) {
+                            throw { msg: "then函数不可以返回自身" }
+                        }
+                        if (temp instanceof MyPromise) {
+                            temp.then(resolve, reject)
+                        } else {
+                            resolve(temp)
+                        }
+                    } catch (err) {
+                        reject(err)
+                    }
+
+                })
+            }
+            switch (this.PromiseState) {
+                case REJECTED: {
+                    callbackPromiseHandler(onRejected)
+                    break;
+                }
+                case PENDING: {
+                    this.fullfulledCallbacks.push(callbackPromiseHandler.bind(this, onFulfilled))
+                    this.rejectedCallbacks.push(callbackPromiseHandler.bind(this, onRejected))
+                    break;
+                }
+                case FULLFILLED: {
+                    callbackPromiseHandler(onFulfilled)
+                    break;
+                }
+            }
+        })
+        return promiseThen
     }
 }
